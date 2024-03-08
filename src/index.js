53bbfed6a79b927e481434ca1a52d91509523e5d
jesse <31576321+agentjesse@users.noreply.github.com>
2024-03-07 22:14:29 -0500

    changed projects and todos to be made with IDs

--- src/index.js
+++ src/index.js
@@ -8,28 +8,29 @@ import { logToConsole as lg, tableToConsole as tb} from "./logger"; //shorthand 
 //functionality for objects
 
 //project objects
-//have: title, notes, due date, project ID
-//do: create todo objects, offer completed todos removal
+//have: project ID, title, description, 
+//do: store and create todo objects, remove completed todos
 const makeProject = projectID=> {
-  let title = 'Untitled Project', description = '';
+  let title = 'Untitled Project', description = '', todoID = 0;
   const todosArr = [];
 
-  const addTodo = ()=> { //keep here and use closure. no need to make another fn to call it with the todosArr array.
-    todosArr.push( makeTodo(todosArr.length) ); //control via index
+  const addTodo = ()=> { //keep here and use closure.
+    todosArr.push( makeTodo(todoID) ); //control via an ID
+    todoID++;
   }
-  const removeCompletedTodos = (...removalIndexes)=> { //pass in indexes of todos to delete
-    removalIndexes.forEach( removalIndex=>{
+  const removeCompletedTodos = (...removalIDs)=> { //pass in number type IDs of todos to delete
+    removalIDs.forEach( removalID=>{
       for ( let currentTodoIndex = 0; currentTodoIndex<todosArr.length; currentTodoIndex++ ){
-        //iterate through each todo until its index matches removalindex and prepare it for deletion
-        if ( todosArr[currentTodoIndex].index === removalIndex ){
+        //iterate through each todo until its ID matches removalID
+        if ( todosArr[currentTodoIndex].getID() === removalID ){
           todosArr.splice(currentTodoIndex,1);
-          currentTodoIndex--; //reposition currentTodoIndex after in place removal
-          break;
+          currentTodoIndex--; //reposition currentTodoIndex after an in place removal
+          break; //no need to keep going after a match and delete
         }
       }
     });
   }
-  return { //public exposure. projectID is not modified
+  return { //public exposure. projectID is passed in and exposed, unmodified
     getProjectID: ()=> projectID,
     getTitle: ()=> title,
     getDescription: ()=> description,
@@ -40,12 +41,13 @@ const makeProject = projectID=> {
 }
 
 //todo objects
-//make with: index position in their array
+//made by passing in a number type argument for ID
 //have: title, notes, due date/time, priority, completion state
-const makeTodo = index=> {
+const makeTodo = id=> {
   let title = 'Untitled Todo', dueDate = '', dueTime = '', notes = '', priority = 'normal', completed = false;
+  
   return { //FIX MEE!!! these props are not closures...
-    index,
+    getID: ()=> id,
     title,
     notes,
     dueDate,
@@ -62,7 +64,7 @@ const makeTodo = index=> {
   let projectID = 0;
   //if local storage is devoid of projects
   //localStorage checking logic goes here...
-  //...make a new one with ID from a counter and push it inside:
+  //...make a new one with ID from a counter, push it in, and increment ID counter:
   projectsArr.push( makeProject(projectID) );
   projectID++;
   projectsArr.push( makeProject(projectID) );
@@ -71,15 +73,16 @@ const makeTodo = index=> {
   //save projects to localStorage....instead of deep cloning, need to save only necessary objects and use them to build new ones after. below is useless shallow clone
   localStorage.setItem( '[TBD]projects in this device\'s localStorage: ', JSON.stringify(projectsArr) );
 
-  //todo testing: add some todos to first project, remove some by index, log project todos
-  for (let runs = 1; runs<=5; runs++) { projectsArr[0].addTodo() };
-  projectsArr[0].removeCompletedTodos(0,1);
-  // tb( projectsArr[0].getTodosArr() );
 
-  // multiple projects testing: log all of them, get id of second one
-  lg( 'ALL PROJECTS: ' )
-  lg( projectsArr )
-  //lg( projectsArr[1].getProjectID() )
+  
+  //todo testing
+  lg ( 'making 5 todos in first project...' )
+  for (let runs = 1; runs<=5; runs++) { projectsArr[0].addTodo() };
+  lg( 'removing index 1 & 3 todos...' )
+  projectsArr[0].removeCompletedTodos(1,3);
+  projectsArr[0].getTodosArr().forEach( (todo, i)=> {
+    lg( `ID of todo at index ${i}: ${todo.getID()}` )
+  });
 
   
 } )();
