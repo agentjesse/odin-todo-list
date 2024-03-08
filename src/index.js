be91ca0ac86d39b7252aff8fc3da0ace07b6b8c8
jesse <31576321+agentjesse@users.noreply.github.com>
2024-03-07 22:14:29 -0500

    started application flow function
    It makes two projects, adds and removes todos to one, then logs all
    projects.

--- src/index.js
+++ src/index.js
@@ -7,16 +7,17 @@ import { logToConsole as lg, tableToConsole as tb} from "./logger"; //shorthand 
 
 //functionality for objects
 
-
 //project objects
-//have: title, notes, due date, array index
+//have: title, notes, due date, project ID
 //do: create todo objects, offer completed todos removal
-const makeProject = projectIndex=> {
-  let title = 'Untitled Project', description = '', todosArr = [];
+const makeProject = projectID=> {
+  let title = 'Untitled Project', description = '';
+  const todosArr = [];
+
   const addTodo = ()=> { //keep here and use closure. no need to make another fn to call it with the todosArr array.
     todosArr.push( makeTodo(todosArr.length) ); //control via index
   }
-  const removeCompletedTodos = (...removalIndexes)=> {
+  const removeCompletedTodos = (...removalIndexes)=> { //pass in indexes of todos to delete
     removalIndexes.forEach( removalIndex=>{
       for ( let currentTodoIndex = 0; currentTodoIndex<todosArr.length; currentTodoIndex++ ){
         //iterate through each todo until its index matches removalindex and prepare it for deletion
@@ -28,10 +29,10 @@ const makeProject = projectIndex=> {
       }
     });
   }
-  return { //public exposure
-    title: ()=> title,
-    description: ()=> description,
-    getProjectIndex: ()=> projectIndex,
+  return { //public exposure. projectID is not modified
+    getProjectID: ()=> projectID,
+    getTitle: ()=> title,
+    getDescription: ()=> description,
     getTodosArr: ()=> todosArr,
     addTodo,
     removeCompletedTodos
@@ -43,7 +44,7 @@ const makeProject = projectIndex=> {
 //have: title, notes, due date/time, priority, completion state
 const makeTodo = index=> {
   let title = 'Untitled Todo', dueDate = '', dueTime = '', notes = '', priority = 'normal', completed = false;
-  return { //these props are not closures.. fix
+  return { //FIX MEE!!! these props are not closures...
     index,
     title,
     notes,
@@ -54,11 +55,31 @@ const makeTodo = index=> {
   }
 }
 
-// testing
-const openARestaurantProj = makeProject();
-for (let runs = 1; runs<=5; runs++) { openARestaurantProj.addTodo() };
-lg('openARestaurantProj object\'s 5 todos:' );
-tb( openARestaurantProj.getTodosArr() );
-openARestaurantProj.removeCompletedTodos(1,3);
-lg('openARestaurantProj object\'s todos array after removal of index 1 & 3 todos:' );
-tb( openARestaurantProj.getTodosArr() );
+//application start IIFE, no referencing for now so skipping assignment
+( ()=>{
+  //store projects in an array that should default to one project if the localStorage doesn't have any
+  const projectsArr = [];
+  let projectID = 0;
+  //if local storage is devoid of projects
+  //localStorage checking logic goes here...
+  //...make a new one with ID from a counter and push it inside:
+  projectsArr.push( makeProject(projectID) );
+  projectID++;
+  projectsArr.push( makeProject(projectID) );
+  projectID++;
+
+  //save projects to localStorage....instead of deep cloning, need to save only necessary objects and use them to build new ones after. below is useless shallow clone
+  localStorage.setItem( '[TBD]projects in this device\'s localStorage: ', JSON.stringify(projectsArr) );
+
+  //todo testing: add some todos to first project, remove some by index, log project todos
+  for (let runs = 1; runs<=5; runs++) { projectsArr[0].addTodo() };
+  projectsArr[0].removeCompletedTodos(0,1);
+  // tb( projectsArr[0].getTodosArr() );
+
+  // multiple projects testing: log all of them, get id of second one
+  lg( 'ALL PROJECTS: ' )
+  lg( projectsArr )
+  //lg( projectsArr[1].getProjectID() )
+
+  
+} )();
